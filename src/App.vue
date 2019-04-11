99194cb5e2feeefd8cb28f28a73471187e921755
shuta13 <hollystarsun@gmail.com>
2019-04-12 02:03:26 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="hoge" @mousemove="onMouseMoved" @touchmove="onMouseMoved">
+      <div class="container" v-bind:style="rotation">
+        <img class="bg" src="./assets/back_image.png">
+        <img class="name-logo" src="./assets/name.png">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      // rotatinoを動的に変化させる
+      rotationX: 0.0,
+      rotationY: 0.0,
+    }
+  },
+  computed: {
+    // これをhtmlに埋め込むとカーソル追跡
+    rotation() {
+      return {
+        // transformは奥行き、rotateでx,y方向にカーソル追跡
+        transform: `perspective(800px) rotateY(${this.rotationX}deg) rotateX(${this.rotationY}deg)`
+      }
+    }
+  },
+  methods: {
+    // カーソルの位置を受け取って
+    onMouseMoved(e) {
+      // 最大の回転の角度
+      const maxRotation = 20.0;
+      // pageX,Yでカーソルの座標eから取得
+      // document.body.client~でクライアントの画面サイズを取得
+      const x = (e.pageX / document.body.clientWidth) * 2 - 1;
+      const y = (e.pageY / document.body.clientHeight) * 2 - 1;
+      this.rotationX = maxRotation * x;
+      this.rotationY = -maxRotation * y;
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  height: 100vh;
+}
+
+#hoge {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+.container {
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  transform-origin: 50%;
+  transform-style: preserve-3d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 100%;
+  /* 直で埋め込んだ */
+  /* transform: perspective(800px); */
+}
+
+.bg {
+  position: absolute;
+  width: 1000px;
+  transform: translateZ(-200px);
+}
+
+.name-logo {
+  position: absolute;
+  opacity: 1;
+  width: 750px;
+  transform: translateZ(100px);
+}
+
+@keyframes vertical {
+    0% { transform:translateY(-10px); }
+  100% { transform:translateY(  0px); }
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  padding: 3px;
 }
 </style>
