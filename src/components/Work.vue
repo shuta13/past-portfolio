c0eb9812ea36e2412e02f81ab13a020c80fec438
shuta13 <hollystarsun@gmail.com>
2019-04-16 11:50:58 +0900

    fixed link

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -48,7 +48,7 @@
             <p>
               EDM, Pops
             </p>
-            <iframe width="360" height="240" src="https://www.youtube.com/embed/3iXUMObwmCU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/568506840&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             <a href="https://soundcloud.com/user-858183512">Soundcloud</a><br>
             <a href="https://piapro.jp/did0es">Piapro</a>
           </div>
