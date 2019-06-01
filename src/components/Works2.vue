5ce9dc9f259f4e34a444d1ffc6c73ddd32f09e86
shuta13 <hollystarsun@gmail.com>
2019-06-01 17:01:25 +0900

    updated some node packages

--- src/components/Works2.vue
+++ src/components/Works2.vue
@@ -46,7 +46,7 @@
           <div class="item">
             <span>music</span>
             <p>
-              EDM, Pops
+              EDM Pops
             </p>
             <iframe scrolling="no" frameborder="no" allow="autoplay" 
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/568506840&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
